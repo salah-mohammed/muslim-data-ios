72f6b3513447e9b95aadfdef7d2ca02f88b1ff53
Salah <Salahalimohamed1995@gmail.com>
2021-01-01 05:39:03 +0200

    no message

--- MuslimData/Classes/Azkars/ArbawnNawawia.swift
+++ MuslimData/Classes/Azkars/ArbawnNawawia.swift
@@ -14,22 +14,31 @@ public struct ArbawnNawawia: FetchableRecord {
 
     public let id: Int
     public let hadeeth: String
+    public let title: String
+    public let reference: String
 
     /// get columns from db.
     enum Columns {
         static let id = Column("id")
         static let hadeeth = Column("hadeeth")
+        static let title = Column("title")
+        static let reference = Column("reference")
+
     }
 
     // MARK: - Constructors
 
-    public init(id: Int, hadeeth: String) {
+    public init(id: Int, hadeeth: String,title:String,reference:String) {
         self.id = id
         self.hadeeth = hadeeth
+        self.title = title
+        self.reference = reference
     }
 
     public init(row: Row) {
         id = row[Columns.id]
         hadeeth = row[Columns.hadeeth]
+        title = row[Columns.title]
+        reference = row[Columns.reference]
     }
 }
