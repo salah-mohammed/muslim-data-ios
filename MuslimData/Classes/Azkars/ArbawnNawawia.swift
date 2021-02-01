5d255ea42fa05a64f3d18d3a226a862a3b8afc07
Salah <Salahalimohamed1995@gmail.com>
2021-02-01 23:09:54 +0200

    no message

--- MuslimData/Classes/Azkars/ArbawnNawawia.swift
+++ MuslimData/Classes/Azkars/ArbawnNawawia.swift
@@ -16,23 +16,29 @@ public struct ArbawnNawawia: FetchableRecord {
     public let hadeeth: String
     public let title: String
     public let reference: String
-
+    public let referenceWithoutForming: String
+    public let titleWithoutForming: String
+    
     /// get columns from db.
     enum Columns {
         static let id = Column("id")
         static let hadeeth = Column("hadeeth")
         static let title = Column("title")
         static let reference = Column("reference")
+        static let titleWithoutForming = Column("titleWithoutForming")
+        static let referenceWithoutForming = Column("referenceWithoutForming")
 
     }
 
     // MARK: - Constructors
 
-    public init(id: Int, hadeeth: String,title:String,reference:String) {
+    public init(id: Int, hadeeth: String,title:String,reference:String,titleWithoutForming:String,referenceWithoutForming:String) {
         self.id = id
         self.hadeeth = hadeeth
         self.title = title
         self.reference = reference
+        self.titleWithoutForming=titleWithoutForming
+        self.referenceWithoutForming=referenceWithoutForming
     }
 
     public init(row: Row) {
@@ -40,5 +46,8 @@ public struct ArbawnNawawia: FetchableRecord {
         hadeeth = row[Columns.hadeeth]
         title = row[Columns.title]
         reference = row[Columns.reference]
+        titleWithoutForming = row[Columns.titleWithoutForming]
+        referenceWithoutForming = row[Columns.referenceWithoutForming]
+
     }
 }
